3b957d49623fa48df9b2d7eddaf760f9aa448f61
carlynorama <carlynorama@gmail.com>
2023-08-25 10:43:46 -0700

    Initial Commit

---
+++ SerialSessionUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SerialSessionUI
+//
+//  Created by Carlyn Maw on 8/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SerialSessionUI/SerialSessionUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SerialSessionUIApp.swift
+//  SerialSessionUI
+//
+//  Created by Carlyn Maw on 8/25/23.
+//
+
+import SwiftUI
+
+@main
+struct SerialSessionUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
