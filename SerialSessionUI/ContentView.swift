c5bd2d8fc659887cd13bed34f7d4a4c98475961e
carlynorama <carlynorama@gmail.com>
2023-08-25 11:41:15 -0700

    refactor into package working

--- SerialSessionUI/ContentView.swift
+++ SerialSessionUI/ContentView.swift
@@ -1,21 +1,39 @@
 //
 //  ContentView.swift
-//  SerialSessionUI
+//  SerialSession
 //
-//  Created by Carlyn Maw on 8/25/23.
+//  Created by Carlyn Maw on 8/24/23.
 //
 
 import SwiftUI
+import SerialSession
+
+
 
 struct ContentView: View {
+//    @State var echoTest:EchoTest = EchoTest(portName: "/dev/cu.usbmodem1101")
+    @StateObject var serialWriter:SerialSession = SerialSession(portName: "/dev/cu.usbmodem1201")
+    
+    @State var brightness:Double = 0.5
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Slider(value: $brightness) { editing in
+                if !editing {
+                    let val = UInt8(brightness * 255)
+                    print(val)
+                    serialWriter.sendByte(val)
+                }
+            }
         }
         .padding()
+        .onAppear() {
+            serialWriter.pingII()
+            //echoTest.run()
+        }
     }
 }
 
---
+++ SerialSessionUI/ReadView.swift
@@ -0,0 +1,49 @@
+//
+//  ReadView.swift
+//  HelloArduino
+//
+//  Created by Carlyn Maw on 8/24/23.
+//
+
+import SwiftUI
+import SerialSession
+
+struct ReadView: View {
+//    @State var echoTest:EchoTest = EchoTest(portName: "/dev/cu.usbmodem1101")
+    @StateObject var serialReader:SerialSession = SerialSession(portName: "/dev/cu.usbmodem1201")
+    
+    @State var reading:String = ""
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("\(reading)")
+            Button("Read") {
+                updateText()
+            }
+        }
+        .padding()
+        .onAppear() {
+            serialReader.pingII()
+            //echoTest.run()
+        }
+    }
+    
+    func updateText() {
+        //let result = serialReader.readLine()
+        let result = serialReader.readBytes(count: 256)
+        switch result {
+        case .success(let message):
+            reading = String(data: message, encoding: .utf8)!
+        case .failure(let error):
+            print(error)
+        }
+    }
+}
+
+struct ReadView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReadView()
+    }
+}
